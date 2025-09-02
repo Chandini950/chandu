7a309619dd0e895c6579a34112aea12531c43346
Chandini950 <chandinimaddi10@gmail.com>
2025-09-02 12:15:05 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,25 @@
+//function first(a)
+//{
+  //  console.log("this is a funtion"+a);
+//}
+//first("call");
+
+//function first(a,b)
+//{
+  //  console.log("addition of two numbers" + a);
+//}
+// first(2,3)
+
+function fn()
+{
+    a=10;
+    console.log("inside the function"+a);
+}
+function fnn()
+{
+    let a=11
+    console.log("2nd function value"+a);
+}
+fn();
+fnn();
+console.log("outside a value"+a);
